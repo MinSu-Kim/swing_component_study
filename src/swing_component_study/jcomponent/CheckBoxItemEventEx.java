b053875dff4e3582f2f79098206a143ca51c29cc
MinSu-Kim <net94.teacher@gmail.com>
2018-11-06 17:22:28 +0900

    체크박스 ItemListener 추가

---
+++ src/swing_component_study/jcomponent/CheckBoxItemEventEx.java
@@ -0,0 +1,121 @@
+package swing_component_study.jcomponent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+
+@SuppressWarnings("serial")
+public class CheckBoxItemEventEx extends JFrame implements ItemListener {
+
+	private JPanel contentPane;
+	private JCheckBox chkApple;
+	private JCheckBox chkPear;
+	private JCheckBox chkCherry;
+	private int sum;
+	private JLabel lblSum;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CheckBoxItemEventEx frame = new CheckBoxItemEventEx();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CheckBoxItemEventEx() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 205);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 10));
+
+		JLabel lblTitle = new JLabel("사과 100원, 배 500원, 체리 2000원");
+		lblTitle.setFont(new Font("굴림", Font.BOLD, 20));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblTitle);
+
+		JPanel pChkBoxes = new JPanel();
+		contentPane.add(pChkBoxes);
+		pChkBoxes.setLayout(new GridLayout(0, 3, 10, 0));
+
+		chkApple = new JCheckBox("사과");
+		chkApple.addItemListener(this);
+		pChkBoxes.add(chkApple);
+
+		chkPear = new JCheckBox("배");
+		chkPear.addItemListener(this);
+		pChkBoxes.add(chkPear);
+
+		chkCherry = new JCheckBox("체리");
+		chkCherry.addItemListener(this);
+		pChkBoxes.add(chkCherry);
+
+		lblSum = new JLabel("현재 0원 입니다");
+		lblSum.setFont(new Font("굴림", Font.BOLD, 20));
+		lblSum.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblSum);
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getSource() == chkCherry) {
+			do_chkCherry_itemStateChanged(e);
+		}
+		if (e.getSource() == chkPear) {
+			do_chkPear_itemStateChanged(e);
+		}
+		if (e.getSource() == chkApple) {
+			do_chkApple_itemStateChanged(e);
+		}
+		lblSum.setText("현재 " + sum + "원 입니다");
+	}
+
+	protected void do_chkApple_itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			sum += 100;
+		} else {
+			sum -= 100;
+		}
+	}
+
+	protected void do_chkPear_itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			sum += 500;
+		} else {
+			sum -= 500;
+		}
+	}
+
+	protected void do_chkCherry_itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			sum += 2000;
+		} else {
+			sum -= 2000;
+		}
+	}
+}
---
+++ src/swing_component_study/jcomponent/JButtonEx.java
@@ -0,0 +1,57 @@
+package swing_component_study.jcomponent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class JButtonEx extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JButtonEx frame = new JButtonEx();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JButtonEx() {
+		initComponents();
+	}
+	private void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 189);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		String imgPath = System.getProperty("user.dir") + "\\images\\";
+		ImageIcon normalIcon = new ImageIcon(imgPath + "normalIcon.gif");
+		ImageIcon rolloverIcon = new ImageIcon(imgPath + "rolloverIcon.gif");
+		ImageIcon pressedIcon = new ImageIcon(imgPath + "pressedIcon.gif");
+		
+		JButton btn = new JButton("call~~", normalIcon);
+		btn.setPressedIcon(pressedIcon);
+		btn.setRolloverIcon(rolloverIcon);
+		contentPane.add(btn, BorderLayout.CENTER);
+	}
+
+}
